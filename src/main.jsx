5f9e167cede1f3193591317188f4ea9fe4a78445
alqstan6-debug <alqstan6@gmail.com>
2026-02-23 20:31:41 +0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Blue Root</h1>
+      <p>Project is working ✅</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
